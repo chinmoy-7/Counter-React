534d42597f0d3bacca3e397058edcc4118a86460
Chinmoy <chinmoy.dehingia@gmail.com>
2022-12-08 23:44:03 +0530

    Functionality is added

--- counter/src/counter.jsx
+++ counter/src/counter.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import "./counter.css";
 const Counter = () => {
+    const [count,setCount]=useState(0);
+    const handleClick = (para)=>{
+        switch(para){
+            case "Increment":
+                setCount(count+1);
+                break;
+            case "Decrement":
+                if(count<=0){
+                    alert("Cannot be less than 0");
+                }else{
+                setCount(count-1);
+                }
+                break;
+        }
+    }
   return (
     <>
     <h1>Counter app</h1>
       <div className="counter-container">
         <div className="buttons">
-          <button>+</button>
-          <button>-</button>
+          <button onClick={(e)=>{handleClick("Increment")}}>+</button>
+          <button onClick={(e)=>{handleClick("Decrement")}}>-</button>
         </div>
         <div className="output">
-            <p>1</p>
+            <p>{count}</p>
         </div>
       </div>
     </>
